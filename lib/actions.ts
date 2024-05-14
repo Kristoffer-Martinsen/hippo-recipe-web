cb89337badaa010c3f1cc4907444e6705a939d3b
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2024-05-14 15:49:36 +0200

    post to db success

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,8 +2,15 @@ export async function createRecipeAction(formData: FormData) {
   console.log(formData);
   const recipeName = formData.get("recipe") as string;
   const description = formData.get("description") as string;
-  const ingredients = formData.getAll("ingredients") as string[];
-  const steps = formData.getAll("steps") as string[];
+  const ingredientsList = formData.getAll("ingredients") as string[];
+  const ingredients: {name: string; unit: string}[] = ingredientsList.map((ingredient: string) => {
+    const [ name, unit ] = ingredient.split(":");
+    return { name, unit: "g" }
+  }); 
+  const stepsList = formData.getAll("steps") as string[];
+  const steps: { instruction: string }[] = stepsList.map((step: string) => {
+    return { instruction: step };
+  });
   const imageURL = formData.get("imageURL") as string;
   
   const res = await fetch("http://localhost:5037/api/Recipe", {
